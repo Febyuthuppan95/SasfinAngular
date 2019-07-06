7357be0dc39018ae8583638b63bcc74a76a36d39
Unknown <stephanharmse@gmail.com>
2019-07-06 19:10:20 +0200

    A70549
    Changed all code and text references to GenericTemplate and GenericTemplateAngular to SasfinGTS

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to GenericTemplateAngular!');
+    expect(page.getTitleText()).toEqual('Welcome to SasfinGTS!');
   });
 
   afterEach(async () => {
