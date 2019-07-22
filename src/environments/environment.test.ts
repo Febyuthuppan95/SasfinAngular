0e2db749ba93913bfe1982ad1c2e7280e7d8e38c
EMaritz <eathon@lateral.solutions>
2019-07-22 13:52:37 +0200

    Hard overwrite of .test env settings

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  AssetRoute: 'http://197.189.218.50:7777/assets/dist',
-  ImageRoute: 'http://197.189.218.50:7777/assets/dist/images',
+  AssetRoute: 'http://197.189.218.50:8888/assets/dist',
+  ImageRoute: 'http://197.189.218.50:8888/assets/dist/images',
   ApiEndpoint: 'http://197.189.218.50:7777'
 };
