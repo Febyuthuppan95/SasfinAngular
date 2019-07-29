b53b79eb6614dff4f7c46eaa30557ea5153b4f74
sjdaniel10 <sjdaniel6241@gmail.com>
2019-07-29 14:25:37 +0200

    created the userRightsView and added the sidebar entry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,6 +41,8 @@ import { SnackBarComponent } from './components/snack-bar/snack-bar.component';
 import { AccountComponent } from './layouts/account/account.component';
 import {RightService} from './services/Right.Service';
 import { ViewDesignationsRightsListComponent } from './views/main/view-designations-rights-list/view-designations-rights-list.component';
+import { UserRightsListComponent } from './user-rights-list/user-rights-list.component';
+import { ViewUserRightsListComponent } from './views/main/view-user-rights-list/view-user-rights-list.component';
 
 // @ts-ignore
 @NgModule({
@@ -69,7 +71,9 @@ import { ViewDesignationsRightsListComponent } from './views/main/view-designati
     ViewBackgroundsListComponent,
     SnackBarComponent,
     AccountComponent,
-    ViewDesignationsRightsListComponent
+    ViewDesignationsRightsListComponent,
+    UserRightsListComponent,
+    ViewUserRightsListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/views/main/view-user-rights-list/view-user-rights-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-user-rights-list',
+  templateUrl: './view-user-rights-list.component.html',
+  styleUrls: ['./view-user-rights-list.component.scss']
+})
+export class ViewUserRightsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
