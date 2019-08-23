2a11e9ead1ed90b3d87f577999d40e2abb2168de
Ashton Robertson <ashton@lateral.solutions>
2019-08-23 16:20:58 +0200

    updated transaction service

--- src/app/services/Transaction.Service.ts
+++ src/app/services/Transaction.Service.ts
@@ -12,11 +12,32 @@ export class TransactionService {
   constructor(private httpClient: HttpClient) {}
 
   /**
-   * get
+   * list
    */
   public list(requestModel) {
     return new Promise((resolve, reject) => {
       const apiURL = `${environment.ApiEndpoint}/transactions/list`;
+
+      this.httpClient
+        .post(apiURL, requestModel)
+        .toPromise()
+        .then(
+          res => {
+            resolve(res);
+          },
+          msg => {
+            reject(msg);
+          }
+        );
+    });
+  }
+
+  /**
+   * listAttatchments
+   */
+  public listAttatchments(requestModel) {
+    return new Promise((resolve, reject) => {
+      const apiURL = `${environment.ApiEndpoint}/transactions/list/files`;
 
       this.httpClient
         .post(apiURL, requestModel)
