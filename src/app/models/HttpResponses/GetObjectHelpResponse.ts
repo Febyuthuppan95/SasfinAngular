299054a6f9f222da677db07230c3b4a53e307a14
sjdaniel10 <sjdaniel6241@gmail.com>
2019-12-13 11:15:04 +0200

    objectHelp, mainly on sidebar

--- src/app/models/HttpResponses/GetObjectHelpResponse.ts
+++ src/app/models/HttpResponses/GetObjectHelpResponse.ts
@@ -1,5 +1,15 @@
+import {Outcome} from './Outcome';
+
 export class GetObjectHelpResponse {
   name: string;
   description: string;
   objectHelpID: number;
+  slug: string;
+  viewSelector: string;
 }
+
+export interface GetObjectHelpListResponse {
+  objectHelpList: GetObjectHelpResponse[];
+  outcome: Outcome;
+}
+
