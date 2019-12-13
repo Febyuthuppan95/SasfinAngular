57e35ea0586648043869ec93fbe42aa56b277ba6
EMaritz <eathon@lateral.solutions>
2019-12-13 08:08:12 +0200

    progress commit

--- src/app/modules/chat/components/chat-conversation/chat-conversation.component.ts
+++ src/app/modules/chat/components/chat-conversation/chat-conversation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-chat-conversation',
@@ -8,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class ChatConversationComponent implements OnInit {
 
   constructor() { }
+  @Output() dismiss = new EventEmitter<void>();
 
   ngOnInit() {
   }
+  dismissEvent() {
+    this.dismiss.emit();
+  }
+
 
 }
