d4403e102aa52aabb2043af4389ea1deee26ac05
ACoetzer <armand@lateral.solutions>
2020-01-17 14:42:38 +0200

    capture stats

---
+++ src/app/models/HttpResponses/AttachmentStatsResponse.ts
@@ -0,0 +1,12 @@
+import { Outcome } from './Outcome';
+
+export class AttachmentStatsResponse {
+  act: number;
+  tact: number;
+  atbc: number;
+  inc: number;
+  err: number;
+  outcome: Outcome;
+}
+
+
--- src/app/views/main/view-capture-landing/view-capture-landing.component.ts
+++ src/app/views/main/view-capture-landing/view-capture-landing.component.ts
@@ -14,6 +14,7 @@ import { takeUntil } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Subject } from 'rxjs';
 import { CaptureAttachmentResponse, CaptureAttachment } from 'src/app/models/HttpResponses/CaptureAttachmentResponse';
+import { AttachmentStatsResponse } from 'src/app/models/HttpResponses/AttachmentStatsResponse';
 
 @Component({
   selector: 'app-view-capture-landing',
@@ -49,6 +50,12 @@ export class ViewCaptureLandingComponent implements OnInit {
   companyName: string;
   transactionID: number;
   attachmentID: number;
+  ACT: number;
+  TACT: number;
+  ATBC: number;
+  Inc: number;
+  ERR: number;
+
   start = false;
 
   loading = false;
@@ -76,6 +83,8 @@ export class ViewCaptureLandingComponent implements OnInit {
 
     if (this.start) {
       this.loadNextAttachment();
+    } else {
+      this.loadAttachmentStats();
     }
   }
 
@@ -106,6 +115,55 @@ export class ViewCaptureLandingComponent implements OnInit {
       },
       msg => {
         this.loading = false;
+      }
+    );
+  }
+
+  loadAttachmentStats() {
+    const model = {
+      captureID: this.currentUser.userID,
+    };
+    this.transactionService
+    .GetAttatchmentStats(model)
+    .then(
+      (res: AttachmentStatsResponse) => {
+
+        this.ACT = res.act;
+        this.TACT = res.tact;
+        this.ATBC = res.atbc;
+        this.Inc = res.inc;
+        this.ERR = res.err;
+      },
+      msg => {
+        this.loading = false;
+      }
+    );
+  }
+
+  loadCaptureScreen() {
+    const model = {
+      captureID: this.currentUser.userID,
+    };
+    this.transactionService
+    .GetAttatchments(model)
+    .then(
+      (res: CaptureAttachmentResponse) => {
+        this.CaptureInfo = res.captureattachment;
+
+        this.docPath = res.captureattachment.filepath;
+        this.transactionID = res.captureattachment.transactionID;
+        this.attachmentID = res.captureattachment.attachmentID;
+        this.fileType = res.captureattachment.filetype;
+        this.companyID = res.captureattachment.companyID;
+        this.companyName = res.captureattachment.companyName;
+
+        this.docService.loadDocumentToViewer(this.docPath);
+        // tslint:disable-next-line: max-line-length
+        this.transactionService.setCurrentAttachment({ transactionID: this.transactionID, attachmentID: this.attachmentID, docType: this.fileType });
+        this.companyService.setCompany({ companyID: this.companyID, companyName: this.companyName });
+        this.router.navigate(['capture', 'transaction', 'attachment']);
+      },
+      msg => {
       }
     );
   }
