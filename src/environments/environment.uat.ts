354aff25ccd6996847c2fe6072c0d4c406c61ebc
EMaritz <eathon@lateral.solutions>
2020-02-11 09:57:33 +0200

    progress

--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
-  AssetRoute: 'http://197.189.218.50:7779/assets/dist',
-  ImageRoute: 'http://197.189.218.50:7779/assets/dist/images',
-  ApiEndpoint: 'http://197.189.218.50:8889/api/v1.0',
-  ApiBackgroundImages: 'http://197.189.218.50:8889/api/v1.0/public/images/background',
-  ApiProfileImages: 'http://197.189.218.50:8889/api/v1.0/public/images/profile',
-  ApiDocuments: 'http://197.189.218.50:8889/api/v1.0/public/document',
-  WebSocketConnection: 'http://197.189.218.50:8889/api/v1.0/chat',
+  AssetRoute: 'http://197.189.218.50:8889/assets/dist',
+  ImageRoute: 'http://197.189.218.50:8889/assets/dist/images',
+  ApiEndpoint: 'http://197.189.218.50:7778/api/v1.0',
+  ApiBackgroundImages: 'http://197.189.218.50:7778/api/v1.0/public/images/background',
+  ApiProfileImages: 'http://197.189.218.50:7778/api/v1.0/public/images/profile',
+  ApiDocuments: 'http://197.189.218.50:7778/api/v1.0/public/document',
+  WebSocketConnection: 'http://197.189.218.50:7778/api/v1.0/chat',
   Sessions: {
     CaptureData: '69c58eaa-ccfb-4fba-97ad-8672ded5ad33',
     ClaimReportData: '3393945f-b088-4d83-a263-6b9029df64ee',
@@ -21,11 +21,11 @@ export const environment = {
   // We should consider doing this for api endpoints -- Ashton
   api: {
     users: {
-      list: 'http://197.189.218.50:8889/api/v1.0/users/list',
-      create: 'http://197.189.218.50:8889/api/v1.0/users/add',
-      update: 'http://197.189.218.50:8889/api/v1.0/users/update',
-      upload: 'http://197.189.218.50:8889/api/v1.0/users/upload',
-      hub: 'http://197.189.218.50:8889/Communication/Hub'
+      list: 'http://197.189.218.50:7778/api/v1.0/users/list',
+      create: 'http://197.189.218.50:7778/api/v1.0/users/add',
+      update: 'http://197.189.218.50:7778/api/v1.0/users/update',
+      upload: 'http://197.189.218.50:7778/api/v1.0/users/upload',
+      hub: 'http://197.189.218.50:7778/Communication/Hub'
     }
   }
 };
