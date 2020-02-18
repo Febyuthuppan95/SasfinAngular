8b060e09a12cb7382203d56ad74d24f8ddac8d04
EMaritz <eathon@lateral.solutions>
2020-02-18 17:00:12 +0200

    uat

--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  AssetRoute: 'http://197.189.218.50:8889/assets/dist',
-  ImageRoute: 'http://197.189.218.50:8889/assets/dist/images',
-  ApiEndpoint: 'http://197.189.218.50:7779/api/v1.0',
+  AssetRoute: 'http://lateralsolutions.ddns.net/sasfin/assets/dist',
+  ImageRoute: 'http://lateralsolutions.ddns.net/sasfin/assets/dist/images',
+  ApiEndpoint: 'https://lateral.solutions/sasfin/api/api/v1.0',
   ApiBackgroundImages: 'http://197.189.218.50:7779/api/v1.0/public/images/background',
   ApiProfileImages: 'http://197.189.218.50:7779/api/v1.0/public/images/profile',
   ApiDocuments: 'http://197.189.218.50:7779/api/v1.0/public/document',
