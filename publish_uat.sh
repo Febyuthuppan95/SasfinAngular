3ecb26a7b35b4ba37459f62b8b2890c62937c08a
developingash <ashtonrobertson96@gmail.com>
2020-09-17 08:59:04 +0200

    publish scripts

--- publish_uat.sh
+++ publish_uat.sh
@@ -1,4 +1,4 @@
-alias ng="C:\Users\ARobertson\AppData\Roaming\npm\node_modules\@angular\cli\bin\ng"
+alias ng="C:/Users/ARobertson/AppData/Roaming/npm/node_modules/@angular/cli/bin/ng"
 git pull
 ng b -c uat -op dist/source/uat
 cp -i web.config ./dist/source/uat/
