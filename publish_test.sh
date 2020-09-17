06b3a77ff345cdc2189b2bd9b67b6325acc3d373
developingash <ashtonrobertson96@gmail.com>
2020-09-17 09:15:41 +0200

    updated uat script

--- publish_test.sh
+++ publish_test.sh
@@ -1,5 +1,5 @@
 alias ng="C:/Users/ARobertson/AppData/Roaming/npm/node_modules/@angular/cli/bin/ng"
 git pull
-ng b -c test --output-path=dist/source/test
+ng b -c test --output-path=dist/source/test/
 cp -i web.config ./dist/source/test/
 $SHELL
