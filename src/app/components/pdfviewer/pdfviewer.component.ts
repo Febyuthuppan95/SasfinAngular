37c5a07626abf78947ccec3ce0c5307dde3d02b9
Mark Laggar <mark@lateral.solutions.com>
2021-02-25 12:37:34 +0200

    pdfViewer zoom update

--- src/app/components/pdfviewer/pdfviewer.component.ts
+++ src/app/components/pdfviewer/pdfviewer.component.ts
@@ -63,7 +63,9 @@ export class PDFViewerComponent implements OnInit, OnChanges, AfterViewInit {
         preventDefault: true,
         allowIn: [AllowIn.Textarea, AllowIn.Input],
         command: e =>  {
+          if (this.zoom_to <= 1.6) {
           this.zoom_in();
+          }
         }
       },
       {
@@ -71,7 +73,9 @@ export class PDFViewerComponent implements OnInit, OnChanges, AfterViewInit {
         preventDefault: true,
         allowIn: [AllowIn.Textarea, AllowIn.Input],
         command: e => {
+          if (this.zoom_to > 0.8) {
           this.zoom_out();
+          }
         }
       },
       {
@@ -102,13 +106,25 @@ export class PDFViewerComponent implements OnInit, OnChanges, AfterViewInit {
   }
 
   zoom_in() {
+    console.log('ZOOM IN');
+    console.log('currentZoom');
+    console.log(this.zoom_to);
+    if (this.zoom_to <= 1.6) {
     this.zoom_to = this.zoom_to + 0.2;
+    }
+    console.log('zoom_to');
+    console.log(this.zoom_to);
   }
 
   zoom_out() {
-    if (this.zoom_to >= 0.4) {
+    console.log('ZOOM OUT');
+    console.log('currentZoom');
+    console.log(this.zoom_to);
+    if (this.zoom_to > 0.8) {
        this.zoom_to = this.zoom_to - 0.2;
     }
+    console.log('zoom_to');
+    console.log(this.zoom_to);
   }
 }
 
